bbcee4028dcff67034480fcd073135ebf12003e8
PosteruOle <posteru.ole@gmail.com>
2024-01-16 18:47:49 +0100

    Added materials for the 1st week!

---
+++ Nedelja_01/1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+int main(){
+  
+  FILE* in=fopen("ulaz.txt", "r");
+  if(in==NULL){
+    fprintf(stderr,"Greska!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  FILE *out=fopen("izlaz.txt", "w");
+  if(out==NULL){
+    fprintf(stderr,"Greska!\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  char c;
+  //Prepisivanje sadrzaja fajla karakter po karakter!
+  /*
+  while((c=fgetc(in))!=EOF){
+    fputc(c, out);
+  }
+  */
+  char buffer[MAX];
+  while(fgets(buffer, MAX, in)!=NULL){
+      int x=fputs(buffer, out);
+      if(x==EOF){
+        fprintf(stderr,"Greska!\n");
+        exit(EXIT_FAILURE);
+      }
+  }
+  
+  fclose(in);
+  fclose(out);
+
+  return 0;
+}
---
+++ Nedelja_01/README.md
@@ -0,0 +1,5 @@
+# Nedelja 1
+Tema: datoteke, argumenti komandne linije, pseudo slučajni brojevi.
+
+Spisak zadataka za ovu nedelju možete pronaći ovde.
+
